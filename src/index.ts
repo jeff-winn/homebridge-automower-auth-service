1e858b18603a8b7a95a96a7659f1dfa190d9e8c7
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-07-09 15:53:09 -0400

    Adding login flow.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import session from 'express-session';
+
 import { v4 as uuid } from 'uuid';
 
 /**
@@ -7,13 +9,32 @@ import { v4 as uuid } from 'uuid';
 const DEFAULT_SERVER_PORT = 8080;
 
 const app = express();
+app.disable('x-powered-by'); // S5689
+
 const port = process.env.PORT ?? DEFAULT_SERVER_PORT;
 
-app.get('/api/v1/authorize/:id', (req, res) => {
+const redirectUri = process.env.REDIRECT_URI!;
+const clientId = process.env.CLIENT_ID!;
+const clientSecret = process.env.CLIENT_SECRET!;
+
+app.use(session({
+    secret: uuid(),
+    cookie: { secure: true }        
+}));
+
+app.get('/oauth/login/:id', (req, res) => {
+    req.session.id = req.params.id;
+    // "https://api.authentication.husqvarnagroup.dev/v1/oauth2/authorize?client_id=<APP KEY>&redirect_uri=<REDIRECT_URI>"
+
+    const redirect = encodeURI(redirectUri);
+    res.redirect(`https://api.authentication.husqvarnagroup.dev/v1/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirect}`);
+});
+
+app.get('/oauth/notify', (req, res) => {
     const code = req.query.code;
     const state = req.query.state;
+    const id = req.session.id;
 
-    const id = uuid();
     console.log(`[${id}] Code: ${code}`);
     console.log(`[${id}] State: ${state}`);
     
